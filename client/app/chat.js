98c261f339c0dadab486d56f10128f731351d909
abibbs <anthonybibbs@gmail.com>
2015-12-29 02:03:27 -0800

    Implemented users feature

--- client/app/chat.js
+++ client/app/chat.js
@@ -1,21 +1,68 @@
 var socket = io();
+
+$('#chatForm').hide();
+$('#room').hide();
+$('#name').focus();
+$('#urlID').hide();
+
 $('#chatForm').submit(function(){
-  socket.emit('chat message', $('#m').val());
-$('#m').val('');
-  return false;
+	socket.emit('chat message', $('#m').val());
+	$('#m').val('');
+	return false;
 });
-socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
+
+$('#join').click(function() {
+	var name = $('#name').val();
+	if (name != '') {
+		socket.emit('join', name);
+		ready = true;
+		$('#chatForm').show();
+		$('#room').show();
+		$('#m').focus();
+		$('#urlID').show();
+		$('#joinChat').hide();
+	}
+});
+
+$('#name').keypress(function(e) {
+	if (e.which === 13) {
+		var name = $('#name').val();
+		if (name != '') {
+			socket.emit('join',name);
+			ready = true;
+		}
+	}
+});
+
+socket.on('update', function(msg) {
+	if (ready) {
+		$('#messages').append($('<li>').text(msg));
+	}
+});
+
+socket.on('update-people', function(people) {
+	if (ready) {
+		$('#people').empty();
+		$.each(people, function(clientid,name) {
+			$('#people').append($('<li>').text(name));
+		});
+	}
+});
+
+socket.on('chat message', function(who,msg){
+	if (ready) {
+		$('#messages').append($('<li>').text(who + ': ' + msg));
+	}
 });
 
 $('#urlID').submit(function(){
-  socket.emit('url submit', $('#url').val());
-$('#url').val('');
-  return false;
-});
-socket.on('url submit', function(url){
-  //set remote urls
-  console.log('url submit 18 :', url);
-  $('.ifm').attr('src', 'https://www.youtube.com/embed/' + url + '?autoplay=1');
+	socket.emit('url submit', $('#url').val());
+	$('#url').val('');
+	return false;
 });
 
+socket.on('url submit', function(url){
+	//set remote urls
+	console.log('url submit:', url);
+	$('.ifm').attr('src', 'https://www.youtube.com/embed/' + url + '?autoplay=1');
+});
--- server/server.js
+++ server/server.js
@@ -2,12 +2,10 @@ var express = require('express');
 var app = express();
 var socketServer = require('http').createServer(app);
 var io = require('socket.io')(socketServer);
+var path = require('path');
+var people = {};
 
 app.use(express.static(__dirname + './../client'));
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join('/index.html'));
-});
 
 socketServer.listen((process.env.PORT || 3000), function() {
   var host = socketServer.address().address;
@@ -17,19 +15,33 @@ socketServer.listen((process.env.PORT || 3000), function() {
 });
 
 // create socket.io connection
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+io.on('connection', function(socket) {
+
+  socket.on('join', function(name) {
+    people[socket.id] = name;
+    socket.emit('update','You have connected to the server');
+    io.emit('update', name + ' has joined the server.');
+    io.emit('update-people', people);
+    console.log(people[socket.id] + ' has connected!');
   });
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
+    io.emit('chat message', people[socket.id], msg);
+    console.log(people[socket.id] + ': ' + msg);
   });
+
+  socket.on('disconnect', function() {
+    io.emit('update', people[socket.id] + ' has left the server.');
+    delete people[socket.id];
+    io.emit('update-people',people);
+    console.log(people[socket.id] || socket.id + ' has disconnected!');
+  });
+
   socket.on('url submit', function(url){
     io.emit('url submit', url);
-    console.log('url: server', url);
+    console.log(people[socket.id] + ' has submitted a URL: ', url);
   });
+
 });
 
 
